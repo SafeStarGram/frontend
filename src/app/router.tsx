c0e962ba85fd5318a8747b02e2335d7996dbf7ae
wooktori <jjun9503@gmail.com>
2025-08-12 19:32:27 +0900

    feat: 로그인 관련 Redux 세팅

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
+import Login from "../pages/LoginPage/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
   { path: "/", element: <App />, children: [] },
+  { path: "/signup", element: <SignUp />, children: [] },
+  { path: "/login", element: <Login />, children: [] },
 ]);
 
 export default router;
