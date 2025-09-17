998d607f974727f48c1e5bd7feae92e87036cc90
wooktori <jjun9503@gmail.com>
2025-09-17 17:03:39 +0900

    feat: summary color 추가

--- src/components/Home/Summary.tsx
+++ src/components/Home/Summary.tsx
@@ -2,9 +2,21 @@ import { FaAngleRight, FaRegBuilding } from "react-icons/fa";
 
 interface IProps {
   area: string;
+  a?: number;
+  b?: number;
 }
 
-export default function Summary({ area }: IProps) {
+const changeColor = (num: number) => {
+  if (num >= 7) {
+    return "text-red-500";
+  } else if (num >= 3) {
+    return "text-yellow-500";
+  } else {
+    return "text-green-500";
+  }
+};
+
+export default function Summary({ area, a = 0, b = 0 }: IProps) {
   return (
     <div className="flex flex-col gap-3 border-2 border-gray-300 rounded-md p-3">
       <div className="flex justify-between items-center gap-3">
@@ -13,8 +25,14 @@ export default function Summary({ area }: IProps) {
         <FaAngleRight />
       </div>
       <div>
-        <div>7 미조치 안전사항</div>
-        <div>12 최근 위험요소 신고</div>
+        <div className="flex items-center gap-3 mb-2">
+          <span className={`text-2xl font-bold ${changeColor(a)}`}>{a}</span>
+          <span className="text-gray-500">미조치 안전사항</span>
+        </div>
+        <div className="flex items-center gap-3">
+          <span className="font-bold">{b}</span>
+          <span className="text-gray-500">최근 위험요소 신고</span>
+        </div>
       </div>
       <div>
         <button className="bg-gray-200 p-2 w-full rounded-md hover:cursor-pointer hover:bg-gray-300 transition">
